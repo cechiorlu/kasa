bbc6fde9c0566e8d89e32afec0ecd54186649b2d
Chimzi Chiorlu <edwincheal@gmail.com>
2021-11-05 13:32:03 +0100

    update styles

--- public/script.js
+++ public/script.js
@@ -1,40 +1,42 @@
-const socket = io();
-
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid')
-const myVideo = document.createElement('video')
 
-myVideo.muted = true
-
-// pass in user id in future versions
-var peer = new Peer(undefined, {
+const myPeer = new Peer(undefined, {
     path: '/peerjs',
     host: '/',
-    port: '3030'
+    port: '3000'
 });
 
-// let myVideoStream
+let myVideoStream;
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
 
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    // myVideoStream = stream
+    myVideoStream = stream
     addVideoStream(myVideo, stream)
-
-    peer.on('call', function (call) {
-        call.answer(stream);
+    myPeer.on('call', call => {
+        call.answer(stream)
         const video = document.createElement('video')
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream)
         })
-    });
-    
+    })
+
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream)
     })
+
 })
 
-peer.on('open', id => {
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
     socket.emit('join-room', roomId, id)
 })
 
@@ -43,16 +45,22 @@ socket.on("connect_error", (err) => {
 });
 
 const connectToNewUser = (userId, stream) => {
-    var call = peer.call(userId, stream);
-    // const video = document.createElement('video')
-    // call.on('stream', userVideoStream => {
-    //     addVideoStream(video, userVideoStream)
-    // });
+    var call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    });
+
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
 }
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream
-    video.addEventListener('loadmetadata', () => {
+    video.addEventListener('loadedmetadata', () => {
         video.play();
     })
 
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server, {
-    allowRequest: (req, callback) => {
-        callback(null, true);
-    }
-})
-const { v4: uuidv4 } = require('uuid')
-const { ExpressPeerServer } = require('peer')
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-})
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use('/peerjs', peerServer)
+
+const peerServer = ExpressPeerServer(httpServer, {
+    debug: true,
+    path: '/'
+});
+
+app.use('/peerjs', peerServer);
+
+const { v4: uuidv4 } = require('uuid')
+
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`)
 })
@@ -24,14 +29,31 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', socket => {
     socket.on('join-room', async (roomId, userId) => {
-        await socket.join(roomId);
+        await socket.join(roomId)
         try {
             socket.to(roomId).emit('user-connected', userId);
         }
-        catch (error){
+        catch (error) {
             console.error(error)
         }
+        // messages
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        });
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
     })
 })
 
-server.listen(3030)
+peerServer.on('connection', client => {
+
+})
+
+peerServer.on('disconnect', client => {
+
+})
+
+httpServer.listen(process.env.PORT || 3000)
